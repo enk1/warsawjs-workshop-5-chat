776f48ec170c25565c2c5f0c6529f723ce3a56dd
enkidu <grzywaczewski.jakub@gmail.com>
2017-04-23 10:52:49 +0200

    index.js server listen

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.Server(app);
 
+app.get('/', (req, res) => res.sendfile(reslove(__dirname, './index.html')));
+server.listen(PORT, () => console.log([`listening on port ${PORT}`]));
